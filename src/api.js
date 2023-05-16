efcd8fedd25c6d708ccc3f8c24d246900cbdf788
jlorenzo58 <jlorenzo58@icloud.com>
2023-05-16 15:42:55 -0400

    commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
-const router = express.router();
+const router = express.Router();
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
